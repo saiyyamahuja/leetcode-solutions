0a960957893ba1b7398a0c7c20823d747d1dc054
saiyyamahuja <114871082+saiyyamahuja@users.noreply.github.com>
2025-02-11 22:37:19 +0530

    File updated at 11/02/2025, 22:37:19

--- 1910-RemoveAllOccurrencesofaSubstring/1910-RemoveAllOccurrencesofaSubstring.py
+++ 1910-RemoveAllOccurrencesofaSubstring/1910-RemoveAllOccurrencesofaSubstring.py
@@ -1,7 +1,28 @@
 class Solution:
-    def removeOccurrences(self, s: str, part: str) -> str:
+  def reverseKGroup(self, head: ListNode | None, k: int) -> ListNode | None:
+    if not head:
+      return None
 
-        while part in s:
-            s=s.replace(part,"",1)
-        return s
+    tail = head
+    for _ in range(k):
+      if not tail:
+        return head
+      tail = tail.next
+    newHead = self._reverse(head, tail)
+    head.next = self.reverseKGroup(tail, k)
+    return newHead
+
+  def _reverse(
+      self,
+      head: ListNode | None,
+      tail: ListNode | None,
+  ) -> ListNode | None:
+    prev = None
+    curr = head
+    while curr != tail:
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    return prev
         

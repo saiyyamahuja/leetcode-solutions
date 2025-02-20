03e1f5c360a35ff54e33e0c70a4a3435f6b27a45
saiyyamahuja <114871082+saiyyamahuja@users.noreply.github.com>
2025-02-20 10:43:12 +0530

    Create file

---
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        
+        def backtrack(i,n,curr):
+            if n==3:
+                if valid(curr):
+                    res.append(curr)
+                return
+            
+            for idx in range(i+1,len(curr)):
+                temp = curr[:idx]+"."+curr[idx:]
+                backtrack(idx,n+1,temp)
+            return 
+
+        def valid(string):
+            ls = string.split(".")
+            for s in ls:
+                if not s or int(s)>255 or int(s)<0 or (len(s)>1
+                 and s[0]=='0'):
+                    return False
+            return True
+        backtrack(0,0,s)
+        return res

2b7783b37385d6632f1f84ba587d02f21c85091c
saiyyamahuja <114871082+saiyyamahuja@users.noreply.github.com>
2025-06-23 10:22:36 +0530

    Create 1752-CheckifArrayIsSortedandRotated

---
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.py
@@ -0,0 +1,18 @@
+# Last updated: 23/06/2025, 10:22:35
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n=len(nums)
+        for offset in range(n):
+            ls=list()
+            for i in range(offset,n):
+                ls.append(nums[i])
+            for i in range(offset):
+                ls.append(nums[i])
+            is_sorted=True
+            for i in range(n-1):
+                if ls[i]>ls[i+1]:
+                    is_sorted=False
+                    break
+            if is_sorted:
+                return True
+        return False

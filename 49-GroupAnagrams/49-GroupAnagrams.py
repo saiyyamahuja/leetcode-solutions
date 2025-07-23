cbc7a506b97af538ebaa4c4fefbce5fb0e107cd6
saiyyamahuja <114871082+saiyyamahuja@users.noreply.github.com>
2025-07-24 00:47:16 +0530

    File updated at 24/07/2025, 00:47:16

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,48 +1,10 @@
-from collections import defaultdict
-
+# Last updated: 24/07/2025, 00:47:16
 class Solution:
-    def groupAnagrams(self, words):
-        len_words = len(words)
-        
-        
-        #words = ["eat","tea","tan","ate","nat","bat"]
-
-        word_to_freq_map = defaultdict(int)
-        for i in range(0, len_words):
-            word = words[i]
-            word_to_freq_map[word] += 1
-
-
-        #word_to_chrs_map['eat'] = 'aet'
-        #word_to_chrs_map['ate'] = 'aet'
-        #word_to_chrs_map['tea'] = 'aet'
-        #word_to_chrs_map['tan'] = 'ant'
-        #word_to_chrs_map['nat'] = 'ant'
-        #word_to_chrs_map['bat'] = 'abt'
-        #word_to_chrs_map = {
-        #  'eat': 'aet'
-        #  , 'ate': 'aet'
-        #  , 'tea': 'aet'
-        #  , ...
-        #}
-
-        word_to_chrs_map = {}
-        for i in range(0, len_words):
-            word = words[i]
-            word_to_chrs_map[word] = ''.join(sorted(word))
-        
-
-        #chrs_to_words_map['aet'] = ['eat', 'ate', 'tea']
-        #chrs_to_words_map['ant'] = ['tan', 'nat']
-        #chrs_to_words_map['abt'] = ['bat']
-
-        chrs_to_words_map = defaultdict(list)
-        for word in word_to_chrs_map.keys():
-            chrs = word_to_chrs_map[word]
-            chrs_to_words_map[chrs].extend([word] * word_to_freq_map[word])
-
-        ret_val = []
-        for chrs in chrs_to_words_map.keys():
-            ret_val.append(chrs_to_words_map[chrs])
-        
-        return ret_val
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            res[tuple(count)].append(s)
+        return(list(res.values()))

682b13ec77b3b1ed3d3b02ef41a3825d4a7d60f6
saiyyamahuja <114871082+saiyyamahuja@users.noreply.github.com>
2025-07-28 14:24:58 +0530

    File updated at 28/07/2025, 14:23:06

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,8 +1,18 @@
-# Last updated: 27/07/2025, 10:58:35
+# Last updated: 28/07/2025, 14:23:06
 class Solution:
+    def alpnum(self,c):
+        return (ord('A')<=ord(c)<=ord('Z') or ord('a')<=ord(c)<=ord('z') or ord('0')<=ord(c)<=ord('9'))
+
     def isPalindrome(self, s: str) -> bool:
-        newStr = ''
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        l,r=0,len(s)-1
+        while l<r:
+            while l<r and not self.alpnum(s[l]):
+                l+=1
+            while r>l and not self.alpnum(s[r]):
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l,r=l+1,r-1
+        return True
+
+    
